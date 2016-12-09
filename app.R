d7ffa341db5465721d2c568f448ddc36f3d53df0
Rob Sinclair <r.sinclair@btinternet.com>
2016-12-09 13:02:58 +0000

    Update name

--- app.R
+++ app.R
@@ -11,7 +11,7 @@ ui <- dashboardPage(
       menuItem("System Overview Dashboard", tabName = "system", icon = icon("dashboard")),
     menuItem("Mental Health Dashboard", tabName = "mentalhealth", icon = icon("dashboard")),
     menuItem("Privacy, Dignity & Wellbeing", tabName = "pdw", icon = icon("dashboard")),
-    menuItem("Worst Organisations", tabName = "orgs", icon = icon("dashboard")),
+    menuItem("Worst Mental Health Orgs", tabName = "orgs", icon = icon("dashboard")),
     menuItem("Datasets", tabName = "datasets", icon = icon("th")),
     menuItem("Results Views", tabName = "resultsViews", icon = icon("th")),
     menuItem("Mental Health Trusts", tabName = "mentalHealthTrusts", icon = icon("th"))
