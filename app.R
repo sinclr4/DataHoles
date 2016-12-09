5ac6c706c577e67e7f5412b36367071e7e7a2cc4
Rob Sinclair <r.sinclair@btinternet.com>
2016-12-09 15:51:47 +0000

    removed slider control

--- app.R
+++ app.R
@@ -20,19 +20,7 @@ ui <- dashboardPage(
   dashboardBody(
 
     tabItems(
-      tabItem(tabName = "mentalhealth",
-              # Boxes need to be put in a row (or column)
-              fluidRow(
-                box(plotOutput("plot1", height = 250)),
-                
-                box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
-                  
-                )
-              )
-              
-              ),
+
       tabItem(tabName = 'system',
               fluidRow( infoBoxOutput("totalBox")),
               fluidRow( infoBoxOutput("totalDSBox")),
