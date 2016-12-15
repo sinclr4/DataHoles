843937783659628cddfcbebd30962ddeb1d6f2ae
Rob Sinclair <r.sinclair@btinternet.com>
2016-12-15 08:44:02 +0000

    removed new badge

--- dashboardSideBar.R
+++ dashboardSideBar.R
@@ -6,12 +6,9 @@ dashboard_sidebar <-   dashboardSidebar(
     menuItem("Worst Mental Health Orgs", tabName = "orgs", icon = icon("dashboard")),
     menuItem("Datasets", tabName = "datasets", icon = icon("th")),
     menuItem("Results Views", tabName = "resultsViews", icon = icon("th")),
-    menuItem("Mental Health Trusts", tabName = "mentalHealthTrusts", icon = icon("th"), 
-             badgeLabel = "new", badgeColor = "green"),
-    menuItem("All Datasets missing datapoints", tabName = "missingDatasets", icon = icon("th"), 
-             badgeLabel = "new", badgeColor = "green"),
-    menuItem("All Trusts", tabName = "allTrusts", icon = icon("th"), 
-             badgeLabel = "new", badgeColor = "green")
+    menuItem("Mental Health Trusts", tabName = "mentalHealthTrusts", icon = icon("th")),
+    menuItem("All Datasets missing datapoints", tabName = "missingDatasets", icon = icon("th")),
+    menuItem("All Trusts", tabName = "allTrusts", icon = icon("th"))
     
   )
 )
